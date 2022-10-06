ed2e3e4a1e58ec28413ffefd841f846b0d0f05c5
jacare-camabox <ramonlindo2311@gmail.com>
2022-10-06 18:58:38 -0300

    update venda dto

--- src/venda/dto/create-venda.dto.ts
+++ src/venda/dto/create-venda.dto.ts
@@ -1,4 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateVendaDto {
+  @ApiProperty({
+    description: 'User id',
+    example: '69143544192384512394',
+  })
   idVendedor: string;
+  @ApiProperty({
+    description: 'lista de propriedades',
+    example: [
+      {
+        name: 'fanta laranja',
+        image:
+          'https://github.com/images-organization/myimages/blob/main/images-all/imgs/fanta%20uva.png?raw=true',
+        price: '5',
+        nameabv: 'fanta',
+        quantity: 2,
+        description: 'fanta uva lata 350ml',
+      },
+    ],
+  })
   list: [];
 }
